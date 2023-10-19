8e286a343ff21e1d2ba6bbdf7e3b87dd29767db0
Gastón Mitoire <gastonmm@gmail.com>
2023-10-19 04:10:05 -0300

    feat: shop styles

--- src/features/shop/Shop.jsx
+++ src/features/shop/Shop.jsx
@@ -3,6 +3,7 @@ import "./Shop.css";
 
 // eslint-disable-next-line
 export const Shop = ({ products }) => {
+  const [showCart, setShowCart] = useState(false);
   const [cart, setCart] = useState([]);
 
   const addToCart = (product) => {
@@ -28,63 +29,98 @@ export const Shop = ({ products }) => {
   return (
     <>
       <div className="shop">
-        <ul className="product-list">
-          {/* eslint-disable-next-line */}
-          {products.map((product) => (
-            <li key={product.id}>
-              <h3>{product.username}</h3>
-              <p>${product.price}</p>
-              <button
-                className="add"
-                onClick={() => {
-                  addToCart(product);
-                }}
-              >
-                <span>+</span>
-              </button>
-            </li>
-          ))}
-        </ul>
-
-        <div>
-          <h2>Carrito</h2>
-          <ul className="cart-list">
+        <button
+          type="button"
+          className="cart-toggle"
+          onClick={() => {
+            setShowCart(!showCart);
+          }}
+        >
+          Ver carrito ({cart.length})
+        </button>
+        {!showCart && (
+          <ul className="product-list">
             {/* eslint-disable-next-line */}
-            {cart
-              .reduce(
-                (acc, product) => {
-                  const repeated = acc.find((item) => item.id === product.id);
-                  if (repeated) {
-                    repeated.quantity += 1;
-                    return acc;
-                  }
-                  return [...acc, { ...product, quantity: 1 }];
-                },
-
-                []
-              )
-              .map((product) => (
-                <li key={product.id}>
-                  <p>({product.quantity})</p>
-                  <h3>{product.username}</h3>
-                  <p>
-                    $
-                    {product.quantity > 1
-                      ? Math.round(product.price * product.quantity * 100) / 100
-                      : product.price}
-                  </p>
-                  <button
-                    className="remove"
-                    onClick={() => {
-                      removeFromCart(product);
-                    }}
-                  >
-                    <span>🗑️</span>
-                  </button>
-                </li>
-              ))}
+            {products.map((product) => (
+              <li key={product.id}>
+                <h3>{product.username}</h3>
+                <p>${product.price}</p>
+                <button
+                  className="add"
+                  onClick={() => {
+                    addToCart(product);
+                  }}
+                >
+                  <span>+</span>
+                </button>
+              </li>
+            ))}
           </ul>
-        </div>
+        )}
+
+        {showCart && (
+          <>
+            <div className="cart">
+              <h2>Carrito</h2>
+
+              <ul className="cart-list">
+                {/* eslint-disable-next-line */}
+                {cart
+                  .reduce(
+                    (acc, product) => {
+                      const repeated = acc.find(
+                        (item) => item.id === product.id
+                      );
+                      if (repeated) {
+                        repeated.quantity += 1;
+                        return acc;
+                      }
+                      return [...acc, { ...product, quantity: 1 }];
+                    },
+
+                    []
+                  )
+                  .map((product) => (
+                    <li key={product.id}>
+                      <p>({product.quantity})</p>
+                      <h3>{product.username}</h3>
+                      <p>
+                        $
+                        {product.quantity > 1
+                          ? Math.round(product.price * product.quantity * 100) /
+                            100
+                          : product.price}
+                      </p>
+                      <button
+                        className="remove"
+                        onClick={() => {
+                          removeFromCart(product);
+                        }}
+                      >
+                        <span>🗑️</span>
+                      </button>
+                    </li>
+                  ))}
+              </ul>
+
+              <p className="cart-total">
+                Total: $
+                {cart
+                  .reduce(
+                    (acc, product) =>
+                      acc +
+                      (product.quantity > 1
+                        ? Math.round(product.price * product.quantity * 100) /
+                          100
+                        : product.price),
+                    0
+                  )
+                  .toFixed(2)}
+              </p>
+            </div>
+            <button className="confirm">Confirmar compra</button>
+          </>
+        )}
       </div>
     </>
   );
